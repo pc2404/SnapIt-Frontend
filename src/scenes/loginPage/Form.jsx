2f4e1dbea4a903633bc1ef0dd200f0d06db84051
Priyanka Chaurasia <pc24priyanka@gmail.com>
2023-01-18 18:12:29 +0530

    server url updated

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -64,7 +64,8 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      // "http://localhost:3001/auth/register",
+      "https://snapit-bn.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
@@ -79,11 +80,15 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(values),
-    });
+    const loggedInResponse = await fetch(
+      // "http://localhost:3001/auth/login",
+      "https://snapit-bn.onrender.com/auth/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      }
+    );
     const loggedIn = await loggedInResponse.json();
     onSubmitProps.resetForm();
     if (loggedIn) {
